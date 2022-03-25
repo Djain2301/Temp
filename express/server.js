da50d8a631ed06ea2f8833fb4abb8868c156d195
Divyansh Jain <jaindivyansh12@yahoo.in>
2022-03-25 15:55:08 +0530

    Last Commit

--- express/server.js
+++ express/server.js
@@ -1,5 +1,7 @@
 const express=require("express");
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 app.get("/",(request,response)=>{
     response.send("<h2>Welcome to Express Demo</h2>");
 })
@@ -13,23 +15,16 @@ app.get("/home",(req,res)=>{
     let fullname=req.query.fullname;
     res.send("<b>Home Page </b>"+ fullname);
 })
-//localhost:3000/home?fullname=test&age=37     -->caled as query parameters
-
 app.get("/login/:email/:password",(req,res)=>{
     let email=req.params.email;
     let pwd=req.params.password;
     res.send("Login "+email+" "+pwd);
 })
-//localhost:3000/home/m@m.com/pwd
-
 app.get("/form",(req,res)=>{
     res.sendFile(__dirname+"/htmlpages/home.html");
     console.log(__dirname)
 })
-//__dirname--->current directory tak ka path aa jayega
-
 app.post("/register",(req,res)=>{
     let fullname=req.body.fullname;
-    response.send(fullname);
+    res.send(fullname);
 })
-//kisi html form me action me post diya ho or vahan se uthana ho toh use this
